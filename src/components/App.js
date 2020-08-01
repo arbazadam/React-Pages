da5a7d93023a48854111c544bca5de353a390617
arbazadam <42610433+arbazadam@users.noreply.github.com>
2020-08-02 01:52:49 +0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NewApp from './NewComponent.js';
+
+function App() {
+  return (
+    <div>
+      <NewApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Button } from 'reactstrap';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+  } from 'reactstrap';
+  
+
+const Abc=()=>{
+    return(
+        <div><h1>Hello</h1>
+        <Button color="success">secondary</Button>
+        </div>
+    );
+}
+
+export default Abc;
